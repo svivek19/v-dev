9d17f00501ac884546f786e2ccfc92cccb3e8b87
svivek19 <svivek030503@gmail.com>
2025-01-29 18:20:40 +0530

    chore: Update server dependencies and configuration

--- server/server.js
+++ server/server.js
@@ -1,15 +1,28 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
+const uri = process.env.MONGO_URI;
 const PORT = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
-// Connect to MongoDB
 
-mongoose.connect("mongodb://localhost/mern-stack-db");
+mongoose
+  .connect(uri)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+try {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+} catch (error) {
+  console.error("Error starting server:", error);
+  process.exit(1);
+}
