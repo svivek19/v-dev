9940d9abee4de738033dea31438903493ca836da
svivek19 <svivek030503@gmail.com>
2025-01-31 11:41:47 +0530

    db setup and server config

--- server/config/db.js
+++ server/config/db.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database is connected");
   } catch (error) {
     console.log(error);
     process.exit(1);
--- server/server.js
+++ server/server.js
@@ -1,22 +1,15 @@
 const express = require("express");
 const cors = require("cors");
-const mongoose = require("mongoose");
+const connectDB = require("./config/db");
 require("dotenv").config();
 
 const app = express();
-const uri = process.env.MONGO_URI;
-const PORT = process.env.PORT || 5000;
-
 app.use(cors());
 app.use(express.json());
 
-mongoose
-  .connect(uri)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => {
-    console.error("MongoDB connection error:", err.message);
-    process.exit(1);
-  });
+connectDB();
+
+const PORT = process.env.PORT;
 
 try {
   app.listen(PORT, () => {
