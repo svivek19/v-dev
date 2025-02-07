8b243caf8f6b3be0a9668f3f365c47810fbcd2fe
svivek19 <svivek030503@gmail.com>
2025-02-07 17:43:18 +0530

    feat: get comments

--- client/src/components/layouts/QuestionAndAnswers.jsx
+++ client/src/components/layouts/QuestionAndAnswers.jsx
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import Axios from "../../util/Axios";
 import { CodeBlock, dracula } from "react-code-blocks";
+import { PulseLoader } from "react-spinners";
+import avatar from "../../assets/avatar.png";
+import { Link } from "react-router-dom";
 
 const QuestionAndAnswers = () => {
   const [questions, setQuestions] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   function formatLocalDate(utcDate) {
     const date = new Date(utcDate);
-
     const options = {
       year: "numeric",
       month: "long",
@@ -17,17 +20,18 @@ const QuestionAndAnswers = () => {
       second: "2-digit",
       hour12: true,
     };
-
     return date.toLocaleString("en-US", options);
   }
 
   async function getQuestions() {
+    setLoading(true);
     try {
       const response = await Axios.get("/question/get");
       setQuestions(response.data.questions || []);
-      console.log(response.data.questions);
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -37,60 +41,67 @@ const QuestionAndAnswers = () => {
 
   return (
     <div className="w-full">
-      {questions.map((item, index) => (
-        <div
-          key={index}
-          className="border border-amber-400 p-4 rounded-lg mb-4 "
-        >
-          {/* Question */}
-          <div className="mb-4">
-            <div
-              className="text-lg sm:text-xl font-medium break-words"
-              dangerouslySetInnerHTML={{ __html: item.question }}
-            />
-            <p className="text-xs my-2 text-end text-slate-500">
-              {formatLocalDate(item.createdAt)}
-            </p>
-          </div>
-
-          <hr className="text-slate-400" />
-
-          {/* Answer */}
-          <div className="my-2 sm:my-4">
-            <div className="flex justify-between items-center mb-2">
-              <div className="flex items-center gap-2 mb-2">
-                <img
-                  src="https://api.dicebear.com/7.x/avataaars/svg?seed=Felix"
-                  alt="avatar"
-                  className="w-10 h-10 rounded-full"
+      {loading ? (
+        <div className="flex justify-center items-center h-screen">
+          <PulseLoader color="#FFA500" size={15} />
+        </div>
+      ) : (
+        questions.map((item, index) => (
+          <div
+            key={index}
+            className="border border-amber-400 p-4 rounded-lg mb-4"
+          >
+            {/* Question */}
+            <div className="mb-4">
+              <Link to={`/question-details/${item._id}`}>
+                <div
+                  className="text-lg sm:text-xl font-medium break-words hover:text-blue-500"
+                  dangerouslySetInnerHTML={{ __html: item.question }}
                 />
-                <div>
-                  <p className="text-base capitalize font-medium text-green-600">
-                    {item.user.name}
-                  </p>
+                <p className="text-xs my-2 text-end text-slate-500">
+                  {formatLocalDate(item.createdAt)}
+                </p>
+              </Link>
+            </div>
+
+            <hr className="text-slate-400" />
+
+            {/* Answer */}
+            <div className="my-2 sm:my-4">
+              <div className="flex justify-between items-center mb-2">
+                <div className="flex items-center gap-2 mb-2">
+                  <img
+                    src={avatar}
+                    alt="avatar"
+                    className="w-10 h-10 rounded-full border border-gray-500"
+                  />
+                  <div>
+                    <p className="text-base capitalize font-medium text-green-600">
+                      {item.user?.name}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="text-sm sm:text-base break-words text-gray-700">
+                <div
+                  className="text-sm sm:text-base mb-4"
+                  dangerouslySetInnerHTML={{ __html: item.question }}
+                />
+                <div className="w-full overflow-x-auto" id="code-block">
+                  <CodeBlock
+                    text={item.code}
+                    language="javascript"
+                    theme={dracula}
+                    showLineNumbers={true}
+                    wrapLines={true}
+                    className="text-sm sm:text-base md:text-lg"
+                  />
                 </div>
               </div>
             </div>
-            <div className="text-sm sm:text-base break-words text-gray-700">
-              <p>Here's </p>
-              <div
-                className="text-sm sm:text-base mb-4"
-                dangerouslySetInnerHTML={{ __html: item.question }}
-              />
-              <div className="w-full overflow-x-auto" id="code-block">
-                <CodeBlock
-                  text={item.code}
-                  language="javascript"
-                  theme={dracula}
-                  showLineNumbers={true}
-                  wrapLines={true}
-                  className="text-sm sm:text-base md:text-lg"
-                />
-              </div>
-            </div>
           </div>
-        </div>
-      ))}
+        ))
+      )}
     </div>
   );
 };
---
+++ client/src/pages/QuestionDetails.jsx
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from "react";
+import Axios from "../util/Axios";
+import { useParams } from "react-router-dom";
+import { CodeBlock, dracula } from "react-code-blocks";
+import avatar from "../assets/avatar.png";
+
+const QuestionDetails = () => {
+  const { id } = useParams();
+  const [ques, setQues] = useState([]);
+
+  const getQuestion = async () => {
+    try {
+      const response = await Axios.get(`/question/get-obj/${id}`);
+      setQues(response.data.response || []);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getQuestion();
+  }, []);
+
+  function formatLocalDateTime(isoDate) {
+    const date = new Date(isoDate);
+    const options = {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    };
+    return date.toLocaleString(undefined, options);
+  }
+
+  return (
+    <div className="flex flex-col md:flex-row gap-4 p-4">
+      <div className="w-full md:w-2/3">
+        <h2 className="text-xl font-bold mb-2 underline underline-offset-2">
+          Discussion Details
+        </h2>
+        <div
+          className="text-sm sm:text-base mb-4 capitalize font-medium text-red-500"
+          dangerouslySetInnerHTML={{ __html: ques.question }}
+        />
+        <div className=" overflow-x-auto" id="code-block">
+          <CodeBlock
+            text={ques.code}
+            language="javascript"
+            theme={dracula}
+            showLineNumbers={true}
+            wrapLines={true}
+            className="text-sm sm:text-base md:text-lg"
+          />
+        </div>
+
+        <div className="my-6">
+          <h2 className="text-xl font-bold mb-2 underline underline-offset-2">
+            What You Think
+          </h2>
+
+          <div className="flex items-center gap-4">
+            <input
+              type="text"
+              placeholder="Enter your text..."
+              className="flex-1 px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+            <button className="cursor-pointer bg-blue-600 text-white px-6 py-2 rounded-lg hover:bg-blue-700 transition">
+              Submit
+            </button>
+          </div>
+
+          <div className="my-4 h-64 overflow-y-scroll">
+            {ques.suggestions &&
+              ques.suggestions.map((item, i) => (
+                <div key={i} className="mb-4 ">
+                  <div className="flex items-center gap-2 mb-2">
+                    <img
+                      src={avatar}
+                      alt="avatar"
+                      className="w-8 h-8 rounded-full border border-gray-500"
+                    />
+                    <div className="w-full flex items-center justify-between">
+                      <p className="text-base capitalize font-medium text-pink-600">
+                        {item.username}
+                      </p>
+                      <p className="text-sm capitalize text-gray-600">
+                        {formatLocalDateTime(ques.createdAt)}
+                      </p>
+                    </div>
+                  </div>
+                  <p className="text-sm text-gray-700 mt-2">
+                    {item.suggestions}
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Temporibus quas quasi accusantium laboriosam. Accusantium
+                    corrupti iure cum vel quisquam, est enim qui. Consequuntur
+                    nihil ratione officia quidem reprehenderit numquam
+                    temporibus.
+                  </p>
+
+                  <hr className="mt-4 border border-slate-300" />
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full md:w-1/3">
+        <h3 className="text-lg font-bold mb-2">Related Insights</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque vel
+          culpa dolorum ipsam natus a accusantium. Vero alias eius incidunt
+          mollitia saepe quia nisi quod iste beatae, laborum, id vitae!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default QuestionDetails;
